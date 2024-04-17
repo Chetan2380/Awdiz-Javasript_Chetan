266a885fe00822f31005e9bd02705408e7402a37
Chetan2380 <148186723+Chetan2380@users.noreply.github.com>
2024-04-17 19:44:50 +0530

    Update

---
+++ Day-01/index.js
@@ -0,0 +1,52 @@
+// Data 
+// Data is information of anything - Chetan, Virat, Rohit 
+
+
+// Datatypes - There are 4 types of data types as follows:
+// Name - Chetan - String
+// Age  - 25 -  Number 
+// Yes/No     - Boolean 
+// Confused  - Undefined
+
+
+// 1. String  
+// Rukles to write proper Syntax 
+// Declare string in ""or '' or `` like below one: 
+// "Virat Kohli " -> String declared in double quotes
+// 'Welcome' -> String declared in single quotes 
+// `BMW`  -> String declared in back quotes 
+
+
+// 2. Number 
+
+// Syntax - YOu can directly numbers without any quotation mark like 
+// 32 
+// 23
+// 1204793
+// 121148
+// 19724
+
+// 3. Boolean  
+// Syntax - Boolean contains only 2 values which are as follows: 
+
+// True False 
+
+// 4. Undefined 
+// syntax 
+
+
+// Varibles - This a one type of storage where you can save values but the values get changed whenever JS events get triggered
+
+//There are 3 types of variables - var, let, const 
+
+
+
+var Name = "Chetan" 
+var Age = 25
+var CompletedAssignment = true
+var InompleteAssignment = false
+
+console.log(Name)
+console.log(Age)
+console.log(CompletedAssignment)
+console.log(InompleteAssignment)
