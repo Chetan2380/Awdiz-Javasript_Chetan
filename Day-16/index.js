ea363f4699a6b6d37b7c9f2b6582237fae3a9e13
Chetan2380 <148186723+Chetan2380@users.noreply.github.com>
2024-05-16 22:52:51 +0530

    Update

--- Day-16/index.js
+++ Day-16/index.js
@@ -1,21 +1,25 @@
-// console.log("Before settimeout");
+console.log("Before settimeout");
 
-// setTimeout(() => {
-//   console.log("Inside setTimeout handler.");
-// }, 10000);
+setTimeout(() => {
+  console.log("Inside setTimeout handler.");
+}, 10000);
 
-// console.log("After settimeout");
+console.log("After settimeout");
+
+setTimeout(() => {
+  console.log("2nd Inside setTimeout handler.");
+}, 1000);
 
 
 // setInterval(() => {
 //   console.log("Inside set interval.");
 // }, 1000);
 
-console.log("Before");
+// console.log("Before");
 
-setTimeout(() => {
-  console.log("setTimeout");
-}, 10000);
+// setTimeout(() => {
+//   console.log("setTimeout");
+// }, 10000);
 
-console.log("After");
+// console.log("After");
 
